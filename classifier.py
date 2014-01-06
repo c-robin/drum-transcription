5145455cd9b7d1c8c703d167e168b60ee6b3b79b
Alexandre Bérard <eskaev@gmail.com>
2014-01-06 18:29:04 +0100

    Factoring + Latex & google-wiki summary

--- classifier.py
+++ classifier.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python2
+# -*- coding: utf-8 -*-
 
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation
+from sklearn.metrics import recall_score, precision_score, f1_score
 from data import extract_features
-from features import normalize
+from features import normalize, mean
 from collections import defaultdict
 import numpy as np
 import sys
@@ -16,33 +18,33 @@ super_category = {
 }
 
 basic_level = {
-    'bass_drum': ['bd'],
-    'snare_drum': ['sd', 'sd-'],
-    'tom': ['lft', 'lt', 'lmt', 'mt'],
-    'hihat': ['ohh', 'chh'],
-    'cymbal': ['ch1', 'cr2', 'cr1', 'cr5', 'spl2', 'c1', 'c4', 'rc4', 'rc2', 'rc3', 'ch5'],
+    'Bass drum': ['bd'],
+    'Snare drum': ['sd', 'sd-'],
+    'Tom': ['lft', 'lt', 'lmt', 'mt'],
+    'Hi-hat': ['ohh', 'chh'],
+    'Cymbal': ['ch1', 'cr2', 'cr1', 'cr5', 'spl2', 'c1', 'c4', 'rc4', 'rc2', 'rc3', 'ch5'],
 }
 
 sub_category = {
-    'bass_drum': ['bd'],
-    'snare_drum': ['sd', 'sd-'],
-    'lowest_tom': ['lft'],
-    'low_tom': ['lt'],
-    'low_mid_tom': ['lmt'],
-    'mid_tom': ['mt'],
-    'open_hihat': ['ohh'],
-    'closed_hihat': ['chh'],
-    'ride_cymbal': ['ch1', 'rc4', 'rc2', 'rc3', 'ch5'],
-    'crash_cymbal': ['cr2', 'cr1', 'cr5'],
-    'splash_cymbal': ['spl2'],
-    'other_cymbal': ['c1', 'c4'],
+    'Bass drum': ['bd'],
+    'Snare drum': ['sd', 'sd-'],
+    'Lowest tom': ['lft'],
+    'Low tom': ['lt'],
+    'Low mid tom': ['lmt'],
+    'Mid tom': ['mt'],
+    'Open hi-hat': ['ohh'],
+    'Closed hi-hat': ['chh'],
+    'Ride cymbal': ['ch1', 'rc4', 'rc2', 'rc3', 'ch5'],
+    'Crash cymbal': ['cr2', 'cr1', 'cr5'],
+    'Splash cymbal': ['spl2'],
+    'Other cymbal': ['c1', 'c4'],
 }
 
 # Simple taxonomy, with 75% coverage
 gillet = {
-    'bass_drum': ['bd'],
-    'snare_drum': ['sd', 'sd-'],
-    'hihat': ['ohh', 'chh'],
+    'Bass drum': ['bd'],
+    'Snare drum': ['sd', 'sd-'],
+    'Hi-hat': ['ohh', 'chh'],
 }
 
 taxonomies = {'gillet': gillet, 'super': super_category, 'basic': basic_level, 'sub': sub_category}
@@ -56,7 +58,7 @@ try:
 except:
     sys.exit('category must be one of these: %s' % str(list(taxonomies.keys())))
 
-classifiers = ['knn', 'svm']
+classifiers = ['knn', 'svm', '3svm']
 if clf not in classifiers:
     sys.exit('classifier must be one of these: %s' % str(classifiers))
 
@@ -92,51 +94,107 @@ features = np.array(features)
 features = normalize(features)
 labels = np.array(labels)
 
-# TODO add HMM classifier, configure SVM
-if clf == 'knn':
-    classifier = KNeighborsClassifier(n_neighbors=3)
-elif clf == 'svm':
-    C = 2
-    d = features.shape[1]
-    sigma = 1
-    gamma = 1.0/(2*d*sigma**2)
-    classifier = SVC(C=C, gamma=gamma)
-
-def score_func(y_true, y_pred):
-    counts = defaultdict(lambda: [0,0,0])
-    for i, truth in enumerate(y_true):
-        prediction = y_pred[i]
-
-        for instr in prediction:
-            counts[instr][0] += 1
-            if instr in truth:
-                counts[instr][2] += 1
-        for instr in truth:
-            counts[instr][1] += 1
-
-    scores = dict()
-    for instr, (n_predict, n_truth, n_correct) in counts.iteritems():
-        precision = float(n_correct)/n_predict
-        recall = float(n_correct)/n_truth
-        if precision == 0 or recall == 0:
-            f_measure = 0
-        else:
-            f_measure = 2*precision*recall/(precision + recall)
-        scores[instr] = np.array([precision, recall, f_measure])
-    return scores
-
-scores = cross_validation.cross_val_score(classifier,features,labels,score_func,cv=10)
-
-# Compute the means of the scores over all test folds
+# Result summary
 sum_scores = dict()
-for scores_ in scores:
-    for instr, values in scores_.iteritems():
-        values /= len(scores)
-        if instr not in sum_scores:
-            sum_scores[instr] = values
-        else:
-            sum_scores[instr] += values
 
-for instr, (precision, recall, f_measure) in sum_scores.iteritems():
-    print('%s: precision=%.3f, recall=%.3f, F-measure=%.3f' % (instr, precision, recall, f_measure))
+# (C, sigma)
+svm_params = (2, 1)
+#params = {
+#    'Hi-hat': (4, 1),
+#    'Snare drum': (2, 2),
+#    'Bass drum': (2, 4),
+#}
 
+if clf == '3svm':
+    x_values = defaultdict(list)
+    y_values = defaultdict(list)
+    for i, feature in enumerate(features):
+        instruments = labels[i]
+        for instr in taxonomy.keys():
+            x_values[instr].append(feature)
+            y_values[instr].append(1 if instr in instruments else 0)
+
+    def score_func(y_true, y_pred):
+        return (precision_score(y_true,y_pred),
+                recall_score(y_true,y_pred),
+                f1_score(y_true,y_pred))
+        
+    for instr in x_values:
+        X = np.array(x_values[instr])
+        Y = np.array(y_values[instr])
+
+        d = X.shape[1]
+        C, sigma = svm_params
+        gamma = 1.0/(2*d*sigma**2)
+        classifier = SVC(C=C, gamma=gamma)
+
+        scores = cross_validation.cross_val_score(classifier, X, Y, score_func, cv=10)
+        sum_scores[instr] = 100*mean(scores)
+else:
+    if clf == 'knn':
+        classifier = KNeighborsClassifier(n_neighbors=3)
+    elif clf == 'svm':
+        d = features.shape[1]
+        C, sigma = svm_params
+        gamma = 1.0/(2*d*sigma**2)
+        classifier = SVC(C=C, gamma=gamma)
+
+    def score_func(y_true, y_pred):
+        counts = defaultdict(lambda: [0,0,0])
+        for i, truth in enumerate(y_true):
+            prediction = y_pred[i]
+            for instr in prediction:
+                counts[instr][0] += 1
+                if instr in truth:
+                    counts[instr][2] += 1
+            for instr in truth:
+                counts[instr][1] += 1
+        scores = dict()
+        for instr, (n_predict, n_truth, n_correct) in counts.iteritems():
+            if n_predict == 0:
+                precision = 1
+            else:
+                precision = float(n_correct)/n_predict
+            recall = float(n_correct)/n_truth
+            if precision == 0 or recall == 0:
+                f_measure = 0
+            else:
+                f_measure = 2*precision*recall/(precision + recall)
+            scores[instr] = np.array([precision, recall, f_measure])
+        return scores
+
+    scores = cross_validation.cross_val_score(classifier, features, labels, score_func, cv=10)
+
+    # Compute the means of the scores over all folds
+    for scores_ in scores:
+        for instr, values in scores_.iteritems():
+            values *= 100.0/len(scores)
+            if instr not in sum_scores:
+                sum_scores[instr] = values
+            else:
+                sum_scores[instr] += values
+
+# Display the results in human readable form, google wiki markup, and latex
+def latex_row(info):
+    return '%s&%.1f\\%%&%.1f\\%%&%.1f\\%%\\\\\n\\hline\n' % info 
+def wiki_row(info):
+   return '|| %s || %.1f%% || %.1f%% || %.1f%% ||\n' % info
+
+latex_table = '\\begin{tabular}{|c|c|c|c|}\n\\hline\nInstrument&Precision&Recall&F-measure\\\\\n\\hline\n'
+wiki_table = '|| Instrument || Precision || Recall || F-measure ||\n'
+
+average = np.array([0.0, 0.0, 0.0])
+for instr, values in sum_scores.iteritems():
+    average += values/len(sum_scores)
+    info = (instr,) + tuple(values)
+
+    print('%s: precision=%.1f%%, recall=%.1f%%, F-measure=%.1f%%' % info)
+
+    latex_table += latex_row(info)
+    wiki_table += wiki_row(info)
+
+latex_table += latex_row(('Average',) + tuple(average))
+wiki_table += wiki_row(('Average',) + tuple(average))
+latex_table += '\\end{tabular}'
+print('\n' + wiki_table)
+print(latex_table)
